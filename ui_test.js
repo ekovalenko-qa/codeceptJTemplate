4cf63e1d7e2698979c24d1675a21fc304c2d5399
ekovalenko <katia.kovalenko@mail.ru>
2021-02-22 17:21:35 +0600

    add tests

--- ui_test.js
+++ ui_test.js
@@ -1,34 +1,35 @@
-const assert = require('assert');
-
 Feature('Тесты для https://idemo.bspb.ru/');
 
-// Scenario('1. Пользователь может авторизоваться', ({ I, mainPage }) => {
-//     mainPage.login();
-//     I.see('Hello World!');
-// });
-//
-// Scenario('2. Пользователь может получить выписку по счету', ({ I, mainPage, myPage }) => {
-//     mainPage.login();
-//     myPage.getCountStatement();
-//     I.see('Выписка');
-// });
-//
-// Scenario('3. Пользователь может открыть вклад', ({ I, mainPage, myPage, depositsPage }) => {
-//     mainPage.login();
-//     myPage.goToDeposits();
-//     depositsPage.createAccount();
-//     I.see('Вклад открыт.');
-// });
-
-Scenario('4. Пользователь может поменять валюту', ({ I, mainPage, myPage, currencyPage }) => {
+Before(({ mainPage }) => {
     mainPage.login();
-    myPage.goToCurrency();
-    currencyPage.changeCurrency();
-    I.see('Обмен валюты успешно совершен');
 });
 
-// Scenario('5. Пользователь может получить реквизиты зарплатного счета', ({ I, mainPage, myPage }) => {
-//     mainPage.login();
-//     myPage.getMainStatementDetails();
-//     I.see('Реквизиты для безналичного перечисления');
-// });
+Scenario('1. Пользователь может авторизоваться', ({ I}) => {
+    I.see('Hello World!');
+});
+
+Scenario('2. Пользователь может получить выписку по счету', ({ I, myPage }) => {
+    myPage.getCountStatement();
+    I.see('Выписка');
+});
+
+Scenario('3. Пользователь может открыть вклад', ({ I, myPage, depositsPage }) => {
+    myPage.goToDeposits();
+    depositsPage.createAccount();
+    I.see('Вклад открыт.');
+});
+
+Scenario('4. Пользователь может получить реквизиты зарплатного счета', ({ I, myPage }) => {
+    myPage.getMainStatementDetails();
+    I.see('Реквизиты для безналичного перечисления');
+});
+
+Scenario('5. Пользователь может посмотреть персональные предложения', ({ I, myPage }) => {
+    myPage.goToOffer();
+    I.see('Персональные предложения');
+});
+
+Scenario('6. Пользователь может посмотреть условия карты', ({ I, myPage }) => {
+    myPage.goToCards();
+    I.see('Золотая');
+});
